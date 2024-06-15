f8d13106e0623941c835dcd49aa51a0f9729d1f8
bmuradova17974 <Bmuradova17974@ada.edu.az>
2024-06-15 23:47:44 +0400

    finished tasks

---
+++ week1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// Task 1 
+int a = 9000;
+bool b;
+
+if (a >= 1000 && a <= 9999 && (a % 3 == 0 || a % 7 == 0))
+{
+    b = true;
+}
+else
+{
+    b = false;
+}
+
+Console.WriteLine(b);
+
+
+// Task 2
+
+String name = "Bilqeyis";
+
+if (name == "Bilqeyis")
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
+// Task 3
+
+Console.Write("Please enter the number: ");
+
+var c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(c % 10);
